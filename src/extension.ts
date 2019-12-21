9fb7582ab1f38ed55a05e6eaa86cc98e9912d74f
TacticalDan <gorksorf@gmail.com>
2019-12-21 11:07:32 -0800

    [#6] Adding publisher info, ready to publish!

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,7 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { Adjective } from './Adjectives';
 import { Noun } from './Nouns';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	const registerStringReplaceCommand = (command: string, replaceCallback: () => string) => {
 		context.subscriptions.push(
@@ -25,5 +21,4 @@ export function activate(context: vscode.ExtensionContext) {
 	registerStringReplaceCommand("extension.noun", Noun.Random);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
