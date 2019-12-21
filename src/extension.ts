9604151ce3d78835ed6f6b46649d69e95998070a
TacticalDan <gorksorf@gmail.com>
2019-12-21 09:06:50 -0800

    [#4] Removed verbs, added list of nouns to Nouns.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,35 +1,28 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { RandomWord } from './Adjectives';
+import { Adjective } from './Adjectives';
+import { Noun } from './Nouns';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const registerStringReplaceCommand = (command: string, replaceCallback: () => string) => {
+		context.subscriptions.push(
+			vscode.commands.registerCommand(command, () => {
+				const textEditor = vscode.window.activeTextEditor;
+				if (textEditor == null) { return; }
+				textEditor.edit((builder) => {
+					for (const selection of textEditor.selections) {
+						builder.replace(selection, replaceCallback());
+					}
+				});
+			})
+		);
+	};
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "wordgenerator" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-
-		const textEditor = vscode.window.activeTextEditor;
-		if (textEditor == null) { return; }
-		textEditor.edit((builder) => {
-			for (const selection of textEditor.selections) {
-				builder.replace(selection, RandomWord.Adjective());
-			}
-		});
-	});
-
-	context.subscriptions.push(disposable);
+	registerStringReplaceCommand("extension.adjective", Adjective.Random);
+	registerStringReplaceCommand("extension.noun", Noun.Random);
 }
 
 // this method is called when your extension is deactivated
